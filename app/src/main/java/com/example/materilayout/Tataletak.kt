1b16f3263c5216736f08ab47101ceb8cc198c1d3
nabilasaf <nabilasafitri861@gmail.com>
2025-10-16 13:29:53 +0700

    Mendefinisikan variabel gambar menggunakan painterResource kedalam Fungsi Tataletak BoxColumnRow

--- app/src/main/java/com/example/materilayout/Tataletak.kt
+++ app/src/main/java/com/example/materilayout/Tataletak.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -84,6 +85,6 @@ fun TataletakRowColumn(modifier: Modifier) {
 
 @Composable
 fun TataletakBoxColumnRow(modifier: Modifier) {
-
+    val gambar = painterResource(id = R.drawable.gambar)
 }
 
