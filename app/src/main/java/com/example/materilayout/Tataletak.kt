4d810a2abfa41b6c1aa903cbb2fd8fed4c4de075
nabilasaf <nabilasafitri861@gmail.com>
2025-10-16 13:11:09 +0700

    Menambahkan Column yang berisi Row pada Fungsi Tataletak ColumnRow

--- app/src/main/java/com/example/materilayout/Tataletak.kt
+++ app/src/main/java/com/example/materilayout/Tataletak.kt
@@ -50,5 +50,12 @@ fun TataletakBox(modifier: Modifier) {
 
 @Composable
 fun TataletakColumnRow(modifier: Modifier) {
-
+    Column (){
+        Row (modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "Komponen1Baris1")
+            Text(text = "Komponen2Baris1")
+            Text(text = "Komponen3Baris1")
+        }
+    }
 }
