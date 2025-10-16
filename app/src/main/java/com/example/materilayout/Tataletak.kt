445bb049cded16ffa4e09e98fc399355e836b662
nabilasaf <nabilasafitri861@gmail.com>
2025-10-16 13:12:52 +0700

    Menambahkan Row baru di dalam Column pada Fungsi Tataletak ColumnRow

--- app/src/main/java/com/example/materilayout/Tataletak.kt
+++ app/src/main/java/com/example/materilayout/Tataletak.kt
@@ -57,5 +57,11 @@ fun TataletakColumnRow(modifier: Modifier) {
             Text(text = "Komponen2Baris1")
             Text(text = "Komponen3Baris1")
         }
+        Row (modifier = modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.SpaceEvenly){
+            Text(text = "Komponen1Baris2")
+            Text(text = "Komponen2Baris2")
+            Text(text = "Komponen3Baris2")
+        }
     }
 }
